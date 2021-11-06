c352de263cd37eb5dd62c71be7d7e2e86c1e5cca
Nitish Gopinath <nitishgopinath@gmail.com>
2021-11-06 20:12:32 +0530

    init setup + commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Provider } from 'react-redux';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
